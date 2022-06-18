f9927953704736f22a255a5b2f92be03899fd358
Ali Ahmadabadiha <Ahmadabadiha@gmail.com>
2022-06-19 01:56:51 +0430

    null or blank check in search view

--- app/src/main/java/com/example/shopingofmine/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/shopingofmine/ui/home/HomeFragment.kt
@@ -2,9 +2,7 @@ package com.example.shopingofmine.ui.home
 
 import android.app.AlertDialog
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Toast
 import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
@@ -40,7 +38,8 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         _binding = FragmentHomeBinding.bind(view)
         binding.searchView.setOnQueryTextListener(object : androidx.appcompat.widget.SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
-                findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToProductsFragment(null, query))
+                if (!query.isNullOrBlank())
+                    findNavController().navigate(HomeFragmentDirections.actionHomeFragmentToProductsFragment(null, query))
                 return true
             }
 
