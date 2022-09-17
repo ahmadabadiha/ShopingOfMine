1c16236bc819875565156052502906a1749ec602
Ali Ahmadabadiha <Ahmadabadiha@gmail.com>
2022-09-18 01:56:25 +0430

    categories fragment redesigned with parent categories

--- app/src/main/java/com/example/shopingofmine/ui/categories/CategoriesFragment.kt
+++ app/src/main/java/com/example/shopingofmine/ui/categories/CategoriesFragment.kt
@@ -1,28 +1,28 @@
 package com.example.shopingofmine.ui.categories
 
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.core.view.isGone
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.example.shopingofmine.R
+import com.example.shopingofmine.data.model.apimodels.CategoryItem
 import com.example.shopingofmine.data.remote.ResultWrapper
-import com.example.shopingofmine.databinding.FragmentCategoriesBinding
+import com.example.shopingofmine.databinding.FragmentCategories2Binding
 import com.example.shopingofmine.ui.buildAndShowErrorDialog
 import com.example.shopingofmine.ui.collectFlow
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CategoriesFragment : Fragment(R.layout.fragment_categories) {
-    private var _binding: FragmentCategoriesBinding? = null
+class CategoriesFragment : Fragment(R.layout.fragment_categories2) {
+    private var _binding: FragmentCategories2Binding? = null
     private val binding get() = _binding!!
     private val viewModel: CategoriesViewModel by viewModels()
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        _binding = FragmentCategoriesBinding.bind(view)
-        val categoriesRecyclerAdapter = CategoriesRecyclerAdapter(::onItemClick)
-        binding.recyclerView.adapter = categoriesRecyclerAdapter
+        _binding = FragmentCategories2Binding.bind(view)
 
         collectFlow(viewModel.categories) {
             when (it) {
@@ -31,7 +31,11 @@ class CategoriesFragment : Fragment(R.layout.fragment_categories) {
                     binding.loadingAnim.isGone = false
                 }
                 is ResultWrapper.Success -> {
-                    categoriesRecyclerAdapter.submitList(it.value)
+                    val categories = it.value
+                    val groupedCategories = groupCategories(categories)
+                    val categoriesRecyclerAdapter = CategoriesRecyclerAdapter2(::onItemClick, groupedCategories)
+                    binding.recyclerView.adapter = categoriesRecyclerAdapter
+                    categoriesRecyclerAdapter.submitList(groupedCategories[0])
                     binding.recyclerView.isGone = false
                     binding.loadingAnim.pauseAnimation()
                     binding.loadingAnim.isGone = true
@@ -43,6 +47,21 @@ class CategoriesFragment : Fragment(R.layout.fragment_categories) {
                 }
             }
         }
+    }
+
+    private fun groupCategories(categories: List<CategoryItem>): Map<Int, List<CategoryItem>> {
+        val groupedCategories = categories.groupBy { category ->
+            category.parent
+        }.toMutableMap()
+
+        groupedCategories[0]!!.forEach {
+            Log.d("ahmad", "groupCategories: before if ${it.id}")
+            if (!groupedCategories.containsKey(it.id)) {
+                Log.d("ahmad", "groupCategories: ${it.id}")
+                groupedCategories[it.id] = listOf(it)
+            }
+        }
+        return groupedCategories.toMap()
     }
 
     private fun onItemClick(categoryId: String) {
---
+++ app/src/main/java/com/example/shopingofmine/ui/categories/CategoriesRecyclerAdapter2.kt
@@ -0,0 +1,70 @@
+package com.example.shopingofmine.ui.categories
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.example.shopingofmine.R
+import com.example.shopingofmine.data.model.apimodels.CategoryItem
+import com.example.shopingofmine.databinding.SubCategoryLayoutBinding
+
+
+class CategoriesRecyclerAdapter2(private val onClick: (String) -> Unit, private val groupedCategories: Map<Int, List<CategoryItem>>) :
+    ListAdapter<CategoryItem, CategoriesRecyclerAdapter2.CategoryViewHolder>(CategoryDiffCallback2()) {
+
+    inner class CategoryViewHolder(private val binding: SubCategoryLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        lateinit var parentCategory: CategoryItem
+
+
+        fun fill(item: CategoryItem) {
+
+            parentCategory = item
+
+            binding.apply {
+                Glide.with(root)
+                    .load(item.image.src)
+                    .error(R.drawable.ic_baseline_error_outline_24)
+                    .transition(DrawableTransitionOptions.withCrossFade())
+                    .into(categoryIcon)
+                categoryTitle.text = item.name
+                seeAll.setOnClickListener {
+                    onClick(item.id.toString())
+                }
+                val categoriesRecyclerAdapter = CategoriesRecyclerAdapter(onClick)
+                recyclerView.adapter = categoriesRecyclerAdapter
+                categoriesRecyclerAdapter.submitList(groupedCategories[item.id])
+            }
+
+        }
+
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryViewHolder = CategoryViewHolder(
+        SubCategoryLayoutBinding.inflate(
+            LayoutInflater
+                .from(parent.context), parent, false
+        )
+    )
+
+
+    override fun onBindViewHolder(holder: CategoryViewHolder, position: Int) {
+        holder.fill(getItem(position))
+
+    }
+}
+
+class CategoryDiffCallback2 : DiffUtil.ItemCallback<CategoryItem>() {
+    override fun areItemsTheSame(oldItem: CategoryItem, newItem: CategoryItem): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: CategoryItem, newItem: CategoryItem): Boolean {
+        return oldItem == newItem
+    }
+}
